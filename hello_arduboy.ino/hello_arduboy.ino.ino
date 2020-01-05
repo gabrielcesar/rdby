8dfb91a0bbb35d0e1e27083d93933095bdb54c5e
Gabriel Cesar <gabrielcesar2@gmail.com>
2020-01-05 14:21:28 -0300

    202001051421

--- hello_arduboy.ino/hello_arduboy.ino.ino
+++ hello_arduboy.ino/hello_arduboy.ino.ino
@@ -2,6 +2,10 @@
 #include <Arduboy.h>
 #include "bitmaps.h"
 
+#define ARRAY_LEN(a) (sizeof(a) / sizeof((a)[0]))
+
+#define FRAME_RATE 60
+#define SHOW_FPS
 // make an instance of arduboy used for many functions
 Arduboy arduboy;
 
@@ -10,12 +14,21 @@ Arduboy arduboy;
 #include <ArdBitmap.h>
 ArdBitmap<WIDTH, HEIGHT> ardbitmap;
 
+#ifdef SHOW_FPS
+
+long previousTime = 0;
+uint8_t fps = 0, fpsCounter = 0;
+
+#endif
+
+
 // This function runs once in your game.
 // use it for anything that needs to be set only once in your game.
 void setup() {
   // initiate arduboy instance
-  arduboy.begin();
-  arduboy.setFrameRate(60);
+  //arduboy.begin();
+  arduboy.beginNoLogo();
+  arduboy.setFrameRate(FRAME_RATE);
 }
 
 
@@ -29,8 +42,25 @@ void loop() {
   // first we clear our screen to black
   arduboy.clear();
 
-  ardbitmap.drawCompressed(WIDTH / 2 ,HEIGHT / 2, BOY, WHITE, ALIGN_CENTER, MIRROR_NONE);
+  //ardbitmap.drawCompressedResized(WIDTH / 2, HEIGHT / 2, BOY[arduboy.frameCount % ARRAY_LEN(BOY)], WHITE, ALIGN_CENTER, MIRROR_HORIZONTAL| MIRROR_VERTICAL, (arduboy.frameCount % 100)/50.0);
+  ardbitmap.drawCompressed(WIDTH / 2, HEIGHT / 2, BOY[arduboy.frameCount % ARRAY_LEN(BOY)], WHITE, ALIGN_CENTER, MIRROR_HORIZONTAL| MIRROR_VERTICAL);  
 
+#ifdef SHOW_FPS
+    fpsCounter++;
+    long actualTime = millis();
+    if ((fpsCounter % 30) == 0) {
+      if (previousTime != 0) {
+        fps = (30 * 1000 / (actualTime - previousTime));
+      }
+      previousTime = actualTime;
+      fpsCounter = 0;
+    }
+
+  arduboy.setCursor(116, 4);
+  arduboy.print(fps);
+
+ #endif
+  
   // then we finaly we tell the arduboy to display what we just wrote to the display
   arduboy.display();
 }
